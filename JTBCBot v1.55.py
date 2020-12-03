bd9b7356928b6f5463b57f6f302472e84620bba4
MrPakoras <hartanas8g@gmail.com>
2020-12-03 12:45:52 +0000

    Update JTBCBot v1.55.py

--- JTBCBot v1.55.py
+++ JTBCBot v1.55.py
@@ -23,7 +23,7 @@ for dirName, subdirList, fileList in os.walk(rootDir):
 while True:
     fileinp = int(input("Enter number of video clip you'd like to edit:   "))
     try:
-        file = flist[(fileinp-1)]
+        file = flist[fileinp-1]
         break
     except IndexError:
         print('Error 404. Please try again.')
